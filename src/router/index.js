8aac3e1c2659027b4060c385d5d68fc1313ef241
chenchuanjiang <413125075@qq.com>
2018-05-07 20:20:43 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
-import SliderDel from '@/components/SliderDel/Index'
-import Picker from '@/components/Picker/Index'
 import DatePicker from '@/components/DatePicker/Index'
 Vue.use(Router)
 
@@ -12,16 +10,6 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
-    },
-    {
-      path: '/sliderDel',
-      name: 'SliderDel',
-      component: SliderDel
-    },
-    {
-      path: '/picker',
-      name: 'Picker',
-      component: Picker
     },
     {
       path: '/date/picker',
