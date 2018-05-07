05c1c95761f89ee0c59a266b8039c827f2a9dd6e
chenchuanjiang <413125075@qq.com>
2018-05-07 20:21:22 +0800

    Update index.vue

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,7 +1,5 @@
 <template>
     <div class="index">
-        <router-link to="/sliderDel">右滑显示按钮</router-link>
-        <router-link to="/picker">picker</router-link>
         <router-link :to="{name: 'DatePicker', params: {name: 'chencj'}}">datePicker</router-link>
     </div>
 </template>
